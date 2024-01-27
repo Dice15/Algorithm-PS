6f1b7a1ed9e7a850fabd29937f37c92adde2049f
Dice <102275981+Dice15@users.noreply.github.com>
2024-01-27 14:03:56 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 51.70 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    Array.from({length:input[0]},(_,i)=>i+1).forEach((e)=>console.log("*".repeat(e)));
+});
