6dae94b1210a021cf6fbbb64ac8ada9fa454ab0f
Dice <102275981+Dice15@users.noreply.github.com>
2024-01-27 23:15:15 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,3 +1,4 @@
-function solution(nums, dir) {
-    return dir==="left" ? [...nums.slice(1, nums.length), nums[0]] : [nums[nums.length-1], ...nums.slice(0, nums.length-1)];
+function solution(nums, dir) {  
+    return (dir==="right" ? nums.unshift(nums.pop()) : nums.push(nums.shift())) && nums;
+    
 }
