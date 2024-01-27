b18ddccb909f61ac0acfd8396344c2b52efb423b
Dice <102275981+Dice15@users.noreply.github.com>
2024-01-27 14:25:56 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,8 +1,3 @@
-function solution(n) {
-    const answer=[n];
-    while(n!==1){
-        n=n%2?3*n+1:n/2;
-        answer.push(n);
-    }
-    return answer;
+function solution(n, a=[]) {
+    return n!==1?solution(n%2?3*n+1:n/2, [...a, n]):[...a, 1]
 }
