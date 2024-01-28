1ff6a8eb5a421e8f665534272642414cff811318
Dice <102275981+Dice15@users.noreply.github.com>
2024-01-28 09:10:23 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,3 +1,3 @@
-function solution([y1,m1,d1], [y2,m2,d2]) {
-    return +(`${y1}${m1.toString().padStart(2,'0')}${d1.toString().padStart(2,'0')}`<`${y2}${m2.toString().padStart(2,'0')}${d2.toString().padStart(2,'0')}`);
+function solution(date1, date2) {
+    return +(new Date(date1) < new Date(date2));
 }
