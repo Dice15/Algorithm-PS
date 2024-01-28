db95734b49348513888825d9abf7aa2405ee2aab
Dice <102275981+Dice15@users.noreply.github.com>
2024-01-28 12:08:16 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,3 +1,7 @@
-function solution(list, cnt=0) {
-    return list.some(v=>v!==1) ? solution(list, list.reduce((s,v,i)=>(v!==1 ? (list[i]=~~(v/2), s+1) : s),cnt)) : cnt
+// function solution(list, cnt=0) {
+//     return list.some(v=>v!==1) ? solution(list, list.reduce((s,v,i)=>(v!==1 ? (list[i]=~~(v/2), s+1) : s),cnt)) : cnt
+// }
+
+function solution(list){
+    return list.reduce((s,e)=>s+e.toString(2).length-1,0)
 }
