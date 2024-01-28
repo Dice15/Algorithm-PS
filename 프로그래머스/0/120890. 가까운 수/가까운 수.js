75e78cf046b46490ef00b9342b9416a3179008f8
Dice <102275981+Dice15@users.noreply.github.com>
2024-01-28 15:25:08 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,3 +1,4 @@
 function solution(arr, n) {  
-    return arr.sort((a,b)=>a-b).reduce((r,e)=>Math.abs(n-e)<r[0] ? [Math.abs(n-e), e] : r,[2e18,2e18])[1]
+    return arr.sort((a,b)=>Math.abs(a-n) - Math.abs(b-n) || a-b)[0]
+    //return arr.sort((a,b)=>a-b).reduce((r,e)=>Math.abs(n-e)<r[0] ? [Math.abs(n-e), e] : r,[2e18,2e18])[1]
 }
