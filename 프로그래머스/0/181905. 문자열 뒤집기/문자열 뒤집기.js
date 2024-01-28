376725f8508ec9d54cef9bb6e9d21487b1c82c8e
Dice <102275981+Dice15@users.noreply.github.com>
2024-01-28 11:24:01 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,3 +1,3 @@
 function solution(str, s, e) {
-    return [...str.substr(0,s), ...[...str.substring(s,e+1)].reverse(), ...str.substr(e+1)].join('')
+    return str.substr(0,s)+[...str.substring(s,e+1)].reverse().join('')+str.substr(e+1)
 }
