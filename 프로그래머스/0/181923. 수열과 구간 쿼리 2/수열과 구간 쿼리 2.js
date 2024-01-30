e5c64441a76cae3b8b16724a4b0a6292af4596fe
Dice <102275981+Dice15@users.noreply.github.com>
2024-01-30 13:13:50 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 15.69 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,3 +1,3 @@
 function solution(arr, queries) {
-    return queries.reduce((res,[s,e,k])=>[...res, [...arr.slice(s, e+1), k].sort((a,b)=>a-b).find(e=>e>k) || -1], []);
+    return queries.map(([s,e,k])=>[...arr.slice(s, e+1), k].sort((a,b)=>a-b).find(e=>e>k) || -1, []);
 }
