6ff3940f3ec0f95108716334922fddf00b8e1b2e
Dice <102275981+Dice15@users.noreply.github.com>
2024-02-07 19:33:44 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.09 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int solution(int number, int limit, int power) {
     int answer = 0;
     for(int i=1;i<=number;i++){
-        int cnt=1;
-        for(int j=1;j<=i/2;j++){
-            if(i%j==0) cnt++;
+        int cnt=0;
+        for(int j=1;j*j<=i;j++){
+            if(i%j!=0) continue;
+            if(j*j==i) cnt++;
+            else cnt+=2;
         }
         answer+=cnt>limit ? power : cnt;
     }
