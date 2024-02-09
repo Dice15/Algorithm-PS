08d1bab25992185f7402b3f2c210a59b6675689a
Dice <102275981+Dice15@users.noreply.github.com>
2024-02-10 07:25:43 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer=0;
+    for(;a!=b;answer++){
+        a=(a+1)/2;
+        b=(b+1)/2;
+    }
+    return answer;
+}
