386ba2bcbfd947d0a8a9b36e243be43982c5baa5
Dice <102275981+Dice15@users.noreply.github.com>
2024-02-15 10:16:30 +0900

    [level 2] Title: 더 맵게, Time: 237.66 ms, Memory: 45.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int solution(vector<int> scoville, int K) {
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    for(auto &e : scoville) pq.push(e);
+    
+    int answer=0;
+    while(pq.size()>1 && pq.top()<K){    
+        ll f=pq.top(); pq.pop();
+        ll s=pq.top(); pq.pop();  
+        pq.push(f+s*2);
+        answer++;
+    }
+    return pq.top()<K ? -1 : answer;
+}
