80f72b6615276ad1bf59c69f299edc0ec2c7762e
Dice <102275981+Dice15@users.noreply.github.com>
2024-03-22 22:16:46 +0900

    [level 3] Title: 네트워크, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,65 +1,111 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class SCC{
+class UnionFind{
 public:
-    int graphSize, sccCnt;
-    vector<vector<int>> graph;
-    vector<int> sccNum;
-    vector<int> dfsCnt;
-    vector<int> dfsStk;
+    int size;
+    vector<int> roots;
     
-    SCC(int _graphSize=0) { assign(_graphSize); }
+    UnionFind(int _size) { assign(_size); }
     
-    void assign(int _graphSize) {
-        graphSize=_graphSize;
-        sccCnt=0;
-        graph.assign(_graphSize, vector<int>());
-        sccNum.assign(_graphSize, -1);
-        dfsCnt.assign(graphSize, 0);
-        dfsStk.clear();
+    void assign(int _size) {
+        size=_size;
+        roots.assign(_size, -1); 
     }
     
-    void add_edge(int u, int v) { graph[u].push_back(v); }
-    
-    int dfs(int curr, int &cnt) {
-        int minCnt=dfsCnt[curr]=++cnt;
-        dfsStk.push_back(curr);
-        
-        for(auto &next : graph[curr]){
-            if(dfsCnt[next]==0) minCnt=min(minCnt, dfs(next, cnt));
-            else if(sccNum[next]==-1) minCnt=min(minCnt, dfsCnt[next]);
-        }
-        
-        if(minCnt==dfsCnt[curr]){
-            while(1){
-                int v=dfsStk.back();
-                dfsStk.pop_back();
-                sccNum[v]=sccCnt;
-                if(v==curr) break;
-            }
-            sccCnt++;
-        }
-        return minCnt;
+    int find(int v){
+        if(roots[v]<0) return v;
+        return roots[v]=find(roots[v]);
     }
     
-    int build() {
-        for(int i=0, cnt=0;i<graphSize;i++){
-            if(sccNum[i]==-1) dfs(i, cnt);
-        }
-        return sccCnt;
+    bool merge(int u, int v){
+        u=find(u);
+        v=find(v);
+        if(u==v) return false;
+        roots[v]+=roots[u];
+        roots[u]=v;
+        return true;
     }
 };
 
+
 int solution(int n, vector<vector<int>> computers) {
-    SCC scc(n);
+    UnionFind uf(n);
     
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if(!computers[i][j]) continue;
-            scc.add_edge(i, j);
+            uf.merge(i, j);
         }
     }
     
-    return scc.build();
+    int answer=0;
+    for(auto &root : uf.roots){
+        if(root<0) answer++;
+    }     
+    return answer;
 }
+
+
+// class SCC{
+// public:
+//     int graphSize, sccCnt;
+//     vector<vector<int>> graph;
+//     vector<int> sccNum;
+//     vector<int> dfsCnt;
+//     vector<int> dfsStk;
+    
+//     SCC(int _graphSize=0) { assign(_graphSize); }
+    
+//     void assign(int _graphSize) {
+//         graphSize=_graphSize;
+//         sccCnt=0;
+//         graph.assign(_graphSize, vector<int>());
+//         sccNum.assign(_graphSize, -1);
+//         dfsCnt.assign(graphSize, 0);
+//         dfsStk.clear();
+//     }
+    
+//     void add_edge(int u, int v) { graph[u].push_back(v); }
+    
+//     int dfs(int curr, int &cnt) {
+//         int minCnt=dfsCnt[curr]=++cnt;
+//         dfsStk.push_back(curr);
+        
+//         for(auto &next : graph[curr]){
+//             if(dfsCnt[next]==0) minCnt=min(minCnt, dfs(next, cnt));
+//             else if(sccNum[next]==-1) minCnt=min(minCnt, dfsCnt[next]);
+//         }
+        
+//         if(minCnt==dfsCnt[curr]){
+//             while(1){
+//                 int v=dfsStk.back();
+//                 dfsStk.pop_back();
+//                 sccNum[v]=sccCnt;
+//                 if(v==curr) break;
+//             }
+//             sccCnt++;
+//         }
+//         return minCnt;
+//     }
+    
+//     int build() {
+//         for(int i=0, cnt=0;i<graphSize;i++){
+//             if(sccNum[i]==-1) dfs(i, cnt);
+//         }
+//         return sccCnt;
+//     }
+// };
+
+// int solution(int n, vector<vector<int>> computers) {
+//     SCC scc(n);
+    
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             if(!computers[i][j]) continue;
+//             scc.add_edge(i, j);
+//         }
+//     }
+    
+//     return scc.build();
+// }
